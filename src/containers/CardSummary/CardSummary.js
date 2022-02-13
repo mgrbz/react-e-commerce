8db3fc0bff4fb7ec8fec2043896c869fae6b1c10
mgrbz <mgurbuzy@gmail.com>
2022-02-13 23:09:26 +0300

    cardsummary development

--- src/containers/CardSummary/CardSummary.js
+++ src/containers/CardSummary/CardSummary.js
@@ -7,10 +7,69 @@ import './cardsummary.scss';
 const CardSummary = (props) => {
 
   console.log('cardSummary', props);
+
+  const orderChangeChecked = () => { 
+    
+    console.log('The checkbox was toggled'); 
+    
+  }; 
+
   return (
-    <div>
+    <div className='card-summary'>
       
-      CardSummary
+      <div className='my-orders'>
+        {
+          props.orders != null && props.orders.length >0 ? props.orders.map(order => {
+            return(
+              <div className='order'>
+                <div className='order-checkbox'>
+                  <input type="checkbox" onChange={orderChangeChecked} />
+                </div> 
+
+                <div className='order-image'>
+                  <img src={order.imageName} alt={order.imageName} />
+                </div>
+
+                <div className='order-info'>
+                  <h4>{order.productName}</h4>
+                  <p>{order.deliveryTime}</p>
+                  <p className='order-sell-shop'>{order.sellShop}</p>
+                </div>
+
+                <div className='order-count-content'>
+                  <div className='centerly-count-content decrease-order'>-</div>
+                  <div className='centerly-count-content order-count'>{order.cardCount}</div>
+                  <div className='centerly-count-content increase-order'>+</div>
+                </div>
+
+                <div className='order-price-content'>
+                  <span className='new-price'>{order.newPrice}</span>
+                  <span className='old-price'>{order.oldPrice}</span>
+                </div>
+
+              </div>
+            )
+          })
+          : <div />
+        }
+
+      </div>
+
+      <div className='order-confirm'>
+        <h2>Sipariş Özeti</h2>
+        <div className='confirm-item'>
+          <p>Ürünün Toplamı</p><p>2,344 TL</p>
+        </div>
+        <div className='confirm-item'>
+          <p>Ürünün Toplamı</p><p>2,344 TL</p>
+        </div>
+        <div className='confirm-item'>
+          <p>Ürünün Toplamı</p><p className='c-three'>2,344 TL</p>
+        </div>
+        <div className='confirm-item'>
+          <p>Ürünün Toplamı</p><p className='c-three'>2,344 TL</p>
+        </div>
+      </div>
 
     </div>
   )
@@ -19,7 +78,7 @@ const CardSummary = (props) => {
 
 const mapStateToProps = ({RCard}) => {
   return{
-    RCard
+    orders : RCard.card
   }
 }
 
--- src/reducers/RProducts.js
+++ src/reducers/RProducts.js
@@ -6,32 +6,40 @@ const initialState = {
         imageName: 'https://flone-react.pages.dev/assets/img/product/accessories/6.jpg',
         productName: 'Kemer',
         newPrice: '3,500 TL',
-        oldPrice: '4,000TL',
-        cardCount : 1
+        oldPrice: '4,000 TL',
+        cardCount : 1,
+        deliveryTime :'2 gün içerisinde kartgoya verilecek!',
+        sellShop: 'Gürbüz mağazacılık'
     },
     {
       id: '112',
       imageName: 'https://flone-react.pages.dev/assets/img/product/accessories/6.jpg',
       productName: 'Cüzdan',
       newPrice: '3,500 TL',
-      oldPrice: '4,000TL',
-      cardCount : 1
+      oldPrice: '4,000 TL',
+      cardCount : 1,
+      deliveryTime :'2 gün içerisinde kartgoya verilecek!',
+      sellShop: 'Gürbüz mağazacılık'
     },
     {
       id: '113',
       imageName: 'https://flone-react.pages.dev/assets/img/product/accessories/6.jpg',
       productName: 'Çanta',
       newPrice: '3,500 TL',
-      oldPrice: '4,000TL',
-      cardCount : 1
+      oldPrice: '4,000 TL',
+      cardCount : 1,
+      deliveryTime :'2 gün içerisinde kartgoya verilecek!',
+      sellShop: 'Gürbüz mağazacılık'
     },
     {
       id: '114',
       imageName: 'https://flone-react.pages.dev/assets/img/product/accessories/6.jpg',
       productName: 'Mont',
       newPrice: '3,500 TL',
-      oldPrice: '4,000TL',
-      cardCount : 1
+      oldPrice: '4,000 TL',
+      cardCount : 1,
+      deliveryTime :'2 gün içerisinde kartgoya verilecek!',
+      sellShop: 'Gürbüz mağazacılık'
     }
   ]
 }
